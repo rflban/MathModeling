36cfa66e3fef0744e039bed89a4a5abd4e6bf3b5
rflban <nabiev.alsn@yandex.ru>
2020-04-25 19:24:14 +0300

    add multithreading

--- lab_01/python/src/solve.py
+++ lab_01/python/src/solve.py
@@ -3,6 +3,9 @@
 import bigfloat as bf
 import texttable as tt
 
+from threading import Thread
+from math import ceil
+
 
 class Monomial:
     def __init__(self, power=1, ratio=1):
@@ -10,7 +13,7 @@ class Monomial:
         self.ratio = bf.BigFloat(ratio)
 
     def __str__(self):
-        return '%.5f*x**%d' % (self.ratio, self.power)
+        return '%.5g*x**%d' % (self.ratio, self.power)
 
     def __add__(self, other):
         if type(other) == Monomial:
@@ -49,13 +52,17 @@ class Polynomial:
 
         for item in monomials:
             if type(item) == Monomial:
-                for i in range(len(self.monomials)):
-                    if self.monomials[i].power == item.power:
-                        self.monomials[i] += item
-                        break
+                if Polynomial.do_group:
+                    for i in range(len(self.monomials)):
+                        if self.monomials[i].power == item.power:
+                            self.monomials[i] += item
+                            break
+                    else:
+                        self.monomials.append(item)
                 else:
                     self.monomials.append(item)
 
+    do_group = True
 
     def __str__(self):
         return ' + '.join('%s' % (monomial) for monomial in self.monomials)
@@ -67,10 +74,13 @@ class Polynomial:
             for monomial in other.monomials:
                 copy += monomial
         elif type(other) == Monomial:
-            for i in range(len(copy.monomials)):
-                if copy.monomials[i].power == other.power:
-                    copy.monomials[i] += other
-                    break
+            if Polynomial.do_group:
+                for i in range(len(copy.monomials)):
+                    if copy.monomials[i].power == other.power:
+                        copy.monomials[i] += other
+                        break
+                else:
+                    copy.monomials.append(other)
             else:
                 copy.monomials.append(other)
 
@@ -85,9 +95,27 @@ class Polynomial:
             for i in range(len(polynomial.monomials)):
                 polynomial.monomials[i] *= other
         elif type(other) == Polynomial:
-            for monomial1 in self.monomials:
-                for monomial2 in other.monomials:
-                    polynomial += monomial1 * monomial2
+            # for monomial1 in self.monomials:
+                # for monomial2 in other.monomials:
+                    # polynomial += monomial1 * monomial2
+            if len(self.monomials) > 0:
+                threads = []
+
+                step = ceil(len(self.monomials) / 8)
+                submonomials = [self.monomials[i : i + step] for i in range(0, len(self.monomials), step)]
+
+                for monomials in submonomials:
+                    thread = Polynomial.MulThread()
+                    thread.set(monomials, other.monomials)
+                    threads.append(thread)
+
+                for thread in threads:
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+                    polynomial.monomials += thread.products
+
         else:
             polynomial.monomials = self.monomials.copy()
 
@@ -113,6 +141,30 @@ class Polynomial:
 
         return integrated
 
+    def mintegrate(self):
+        integrated = Polynomial()
+        threads = []
+
+        step = ceil(len(self.monomials) / 8)
+        submonomials = [self.monomials[i : i + step] for i in range(0, len(self.monomials), step)]
+
+        # for ms in submonomials:
+            # print(*ms, sep=' + ')
+
+        for monomials in submonomials:
+            thread = Polynomial.IntegrateThread()
+            thread.set(monomials)
+            threads.append(thread)
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+            integrated.monomials += thread.monomials
+
+        return integrated
+
     def calc(self, x):
         res = 0
 
@@ -120,6 +172,32 @@ class Polynomial:
             res += monomial.calc(x)
 
         return res
+
+    class IntegrateThread(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+
+        def set(self, monomials):
+            self.monomials = monomials
+
+        def run(self):
+            for i in range(len(self.monomials)):
+                self.monomials[i] = self.monomials[i].integrate()
+
+    class MulThread(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+
+        def set(self, factors1, factors2):
+            self.factors1 = factors1
+            self.factors2 = factors2
+
+        def run(self):
+            self.products = []
+
+            for factor1 in self.factors1:
+                for factors2 in self.factors2:
+                    self.products.append(factor1 * factors2)
 
 
 def my_pol(x, u):
@@ -131,7 +209,8 @@ def picard(f, n):
     u = Polynomial()
 
     while n:
-        u = f(x, u).integrate()
+        u = f(x, u).mintegrate()
+        # u = f(x, u).integrate()
         n -= 1
 
     return u
@@ -139,9 +218,10 @@ def picard(f, n):
 
 def main():
     with bf.precision(1000):
-        pol = picard(my_pol, 10)
+        pol = picard(my_pol, 12)
         print(pol)
         print(pol.calc(1))
+    # print(my_pol(Monomial(1, 1), Polynomial()).mintegrate())
 
 
 if __name__ == '__main__':

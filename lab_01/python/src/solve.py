29284cdbe3a8139385619c953212d73dc8561cd8
rflban <nabiev.alsn@yandex.ru>
2020-04-26 18:54:14 +0300

    add not working multiprocessing

--- lab_01/python/src/solve.py
+++ lab_01/python/src/solve.py
@@ -15,8 +15,7 @@ def picard(x, n):
 
         for i in range(len(polynomial)):
             for j in range(len(polynomial)):
-                squared[i + j + 1] += polynomial[i] * polynomial[j]
-                squared[i + j + 1] /= (i + j + 1) * 4 + 3
+                squared[i + j + 1] += polynomial[i] * polynomial[j] / ((i+j+1)*4 + 3)
 
         squared[0] = Decimal(1 / 3)
         polynomial = squared
@@ -35,8 +34,65 @@ def picard(x, n):
     return res
 
 
+def mtpicard(x, n):
+    polynomial = [Decimal(1 / 3)]
+
+    while (n - 1):
+        squared = [Decimal(0)] * (len(polynomial) * 2)
+
+        fullrange = range(len(polynomial))
+
+        step = ceil(len(fullrange) / 8)
+        subranges = [fullrange[i:i+step] for i in range(0, len(fullrange), step)]
+        threads = []
+        # print(subranges)
+
+        for subrange in subranges:
+            thread = PicardThread()
+            thread.squared = squared
+            thread.polynomial = polynomial
+            thread.subrange = subrange
+            threads.append(thread)
+
+        print(len(threads))
+
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+        squared[0] = Decimal(1 / 3)
+        polynomial = squared
+
+        n -= 1
+
+    # print(polynomial)
+    res = Decimal(0)
+    x = Decimal(x)
+
+    for i in range(len(polynomial)):
+        res += polynomial[i] * x ** (i * 4 + 3)
+
+    print((len(polynomial) - 1) * 4 + 3)
+
+    return res
+
+
+class PicardThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        for i in self.subrange:
+            for j in range(len(self.polynomial)):
+                self.squared[i+j+1] += self.polynomial[i] * self.polynomial[j] / ((i+j+1)*4 + 3)
+
+
 def main():
-    print(picard(2, 3))
+    n = 15
+
+    # print(picard(2, n))
+    print(mtpicard(2, n))
 
 
 if __name__ == '__main__':

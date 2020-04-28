3cbe393c1f07f5eb4331f5e2da782e69dbffffd4
rflban <nabiev.alsn@yandex.ru>
2020-04-28 12:37:24 +0300

    rm multithreading picard

--- lab_01/python/src/solve.py
+++ lab_01/python/src/solve.py
@@ -3,8 +3,6 @@
 import texttable as tt
 
 from decimal import Decimal
-from threading import Thread
-from math import ceil
 
 
 def picard(x, n):
@@ -34,65 +32,8 @@ def picard(x, n):
     return res
 
 
-def mtpicard(x, n):
-    polynomial = [Decimal(1 / 3)]
-
-    while (n - 1):
-        squared = [Decimal(0)] * (len(polynomial) * 2)
-
-        fullrange = range(len(polynomial))
-
-        step = ceil(len(fullrange) / 8)
-        subranges = [fullrange[i:i+step] for i in range(0, len(fullrange), step)]
-        threads = []
-        # print(subranges)
-
-        for subrange in subranges:
-            thread = PicardThread()
-            thread.squared = squared
-            thread.polynomial = polynomial
-            thread.subrange = subrange
-            threads.append(thread)
-
-        print(len(threads))
-
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
-
-        squared[0] = Decimal(1 / 3)
-        polynomial = squared
-
-        n -= 1
-
-    # print(polynomial)
-    res = Decimal(0)
-    x = Decimal(x)
-
-    for i in range(len(polynomial)):
-        res += polynomial[i] * x ** (i * 4 + 3)
-
-    print((len(polynomial) - 1) * 4 + 3)
-
-    return res
-
-
-class PicardThread(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-
-    def run(self):
-        for i in self.subrange:
-            for j in range(len(self.polynomial)):
-                self.squared[i+j+1] += self.polynomial[i] * self.polynomial[j] / ((i+j+1)*4 + 3)
-
-
 def main():
-    n = 15
-
-    # print(picard(2, n))
-    print(mtpicard(2, n))
+    print(picard(2, 17))
 
 
 if __name__ == '__main__':
